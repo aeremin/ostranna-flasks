99654a58ca14a8680dbb96931adf8b265e10d26f
Alexey Eremin <a.eremin.msu@gmail.com>
2022-07-12 17:48:44 +0200

    Create template android app

---
+++ client/app/src/main/java/in/aerem/ostranna_flasks/MainActivity.kt
@@ -0,0 +1,18 @@
+package `in`.aerem.ostranna_flasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import `in`.aerem.ostranna_flasks.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ client/app/src/main/java/in/aerem/ostranna_flasks/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package `in`.aerem.ostranna_flasks.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
