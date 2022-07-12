6ed8e7c2e975eb74cda4bce94b708820d763ba69
Alexey Eremin <a.eremin.msu@gmail.com>
2022-07-12 19:02:34 +0200

    Add example firebase realtime database integration

--- client/app/src/main/java/in/aerem/ostranna_flasks/MainActivity.kt
+++ client/app/src/main/java/in/aerem/ostranna_flasks/MainActivity.kt
@@ -3,6 +3,8 @@ package `in`.aerem.ostranna_flasks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import `in`.aerem.ostranna_flasks.ui.main.MainFragment
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +16,7 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, MainFragment())
                     .commitNow()
         }
+
+        Firebase.database.setPersistenceEnabled(true)
     }
 }
--- client/app/src/main/java/in/aerem/ostranna_flasks/ui/main/MainFragment.kt
+++ client/app/src/main/java/in/aerem/ostranna_flasks/ui/main/MainFragment.kt
@@ -9,7 +9,18 @@ import android.view.ViewGroup
 import `in`.aerem.ostranna_flasks.R
 import android.util.Log
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
+
+data class Professor(
+    val name: String = "",
+    val limit: Int = 0,
+)
 
 class MainFragment : Fragment() {
     companion object {
@@ -17,6 +28,7 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private val database = Firebase.database
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -36,9 +48,27 @@ class MainFragment : Fragment() {
                 Log.w(TAG, "Fetching FCM registration token failed", task.exception)
                 return@OnCompleteListener
             }
-            Log.i(TAG, "Fetched a messaging token: ${task.result}")
+            val token = task.result
+            Log.i(TAG, "Fetched a messaging token: $token")
+            database.getReference("devices/$token").setValue("Connected!")
         })
 
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val professors = dataSnapshot.getValue<Map<String, Professor>>()
+                if (professors != null) {
+                    for (professor in professors) {
+                        Log.i(TAG, "Professor ${professor.value.name} (id: ${professor.key}) has limit of ${professor.value.limit}")
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        database.getReference("professors").addValueEventListener(postListener)
     }
 
 }
